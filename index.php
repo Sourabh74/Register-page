38cc69998d8ba3cc666c92cc57757b54c08a15a3
Sourabh74 <76158191+Sourabh74@users.noreply.github.com>
2021-02-20 20:43:40 +0530

    Add files via upload
    html, css, php.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_POST['name'])){
+
+
+  $server = "localhost:3325";
+  $username = "root";
+  $password ="";
+  $dbname ="register";
+  $con = mysqli_connect($server, $username, $password,$dbname);
+
+  if(!$con){
+      die("connection to this database failed due to" . 
+      mysqli_connect_error());
+  }
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $address = $_POST['address'];
+ 
+ $sql = "INSERT INTO `info` (`name`, `email`, `phone`, `address`) VALUES ('$name', '$email', '$phone', '$address'); ";
+ 
+ if($con->query($sql)== true)
+ {
+   echo "succefully sing up";
+ }
+ else{
+   echo "Error: $sql<br> $con->error";
+ }
+ $con->close();
+}
+?>
+
